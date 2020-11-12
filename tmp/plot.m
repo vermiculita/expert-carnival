72a1b68d378eaa8234c3e911b84df38f0c8710aa
vermiculita <vermiculite2020@protonmail.com>
2020-11-12 01:01:37 -0500

    added second plot

--- tmp/plot.m
+++ tmp/plot.m
@@ -1,3 +1,8 @@
 x = dlmread('oakland-mi-subset.csv',',',1,0);
 st_rate = x(:,2)./x(:,3);
+figure(1)
 plot(st_rate(:),x(:,4)./x(:,5)-st_rate(:),'.')
+
+r_rate = (x(:,2)+x(:,4))./(x(:,3)+x(:,5));
+figure(2)
+plot(r_rate(:),x(:,4)./x(:,5)-x(:,2)./x(:,3),'.')
