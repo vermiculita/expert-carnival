8a8b3168059d9ec8c99851c04b4b9d402403a57a
vermiculita <vermiculite2020@protonmail.com>
2020-11-12 01:37:55 -0500

    comment

--- tmp/plot.m
+++ tmp/plot.m
@@ -1,11 +1,42 @@
 x = dlmread('oakland-mi-subset.csv',',',1,0);
+
+%
+% Shiva's plot
+%
+% X axis is the rate of R straight tickets per overall straight ticket
+% voter
+%
+% Y axis is the rate of R indiv trump vote per overall indiv ballots 
+%
+
 st_rate = x(:,2)./x(:,3);
 figure(1)
 plot(st_rate(:),x(:,4)./x(:,5)-st_rate(:),'.')
 
+%
+% alternate 1
+% 
+% X axis is overall rate of trump votes, any source
+%
+% Y axis is difference between rate of R straight ticket per overall
+% straight tickets, and rate of trump votes per overall indiv ballots
+%
+
 r_rate = (x(:,2)+x(:,4))./(x(:,3)+x(:,5));
 figure(2)
 plot(r_rate(:),x(:,4)./x(:,5)-x(:,2)./x(:,3),'.')
+
+%
+% alternate 2
+%
+% X axis is overall rate of trump votes, any source 
+%
+% Y axis is difference between rate of individual trump votes per all votes, 
+% and the rate of R straight ticket votes per all votes
+%
+% shows that as the overall fraction of trump votes increases, the fraction
+% of stright ticket votes increases relative to individual, linearly.
+%
 
 figure(3)
 plot(r_rate(:),x(:,4)./(x(:,3)+x(:,5))-x(:,2)./(x(:,3)+x(:,5)),'.')
