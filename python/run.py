5ef5cb0cd02d9a9c179a9dd5bc6263ecc67aceb3
vermiculita <vermiculite2020@protonmail.com>
2020-11-12 23:09:20 -0500

    fixed a bug and added another county

--- python/run.py
+++ python/run.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # load dataframes from oakland's format
-def load_michigan_data(dirname):
+def load_oakland_data(dirname):
     individual = pd.read_csv("%s/ballot-sheet.csv" % (dirname), skiprows=2)
     individual.rename(columns={
         "Absentee": "D.Absentee",
@@ -27,40 +27,65 @@ def load_michigan_data(dirname):
 
     return (individual, straight)
 
-# compute the total vote counts from both types of ballot
-def compute_totals(i,s):
-    return pd.DataFrame({
-        'total_d': i['D.Total'] + s['D.Total'],
-        'total_r': i['R.Total'] + s['R.Total'],
-        'total_v': i['Total'] + s['Total']
-        })
+# load dataframes from our format
+def load_our_data(dirname):
+    individual = pd.read_csv("%s/data.csv" % (dirname))    
+    straight = pd.read_csv("%s/data.csv" % (dirname))
+    straight.rename(columns={
+        "R.Total": "R.Total.Ind",
+        "D.Total": "D.Total.Ind",
+        "D.Total.Str": "D.Total",
+        "R.Total.Str": "R.Total",
+        }, inplace=True)
 
-def build_plots(i,s,t):
+    return (individual, straight)
+
+def build_plots(i,s):
     return pd.DataFrame({
-        'frac_r': t['total_r'] / t['total_v'],
-        'frac_d': t['total_d'] / t['total_v'],
-        'frac_sr': s['R.Total'] / t['total_v'],
-        'frac_ir': i['R.Total'] / t['total_v'],
-        'frac_sd': s['D.Total'] / t['total_v'],
-        'frac_id': i['D.Total'] / t['total_v'],
+        'frac_r': i['R.Total'] / i['Total'],
+        'frac_d': i['D.Total'] / i['Total'],
+        'frac_sr': s['R.Total'] / i['Total'],
+        'frac_ir': (i['R.Total']-s['R.Total']) / i['Total'],
+        'frac_sd': s['D.Total'] / i['Total'],
+        'frac_id': (i['D.Total']-s['D.Total']) / i['Total'],
         })
 
 def run():
-    (oakland_i, oakland_s) = load_michigan_data('../source-data/oakland-mi-11-11-2020/')
+    (oakland_i, oakland_s) = load_oakland_data('../source-data/oakland-mi-11-11-2020/')
 
-    oakland_totals = compute_totals(oakland_i, oakland_s)
-
-    cons = build_plots(oakland_i, oakland_s, oakland_totals)
+    cons = build_plots(oakland_i, oakland_s)
 
     plt.figure(1)
     ax = plt.gca()
-    cons.plot(kind='scatter', x='frac_r', y='frac_sr', color='red', ax=ax)
-    cons.plot(kind='scatter', x='frac_r', y='frac_ir', color='blue', ax=ax)
-
+    cons.plot(kind='scatter', x='frac_r', y='frac_sr', color='red', ax=ax, label='Fraction Straight Ticket')
+    cons.plot(kind='scatter', x='frac_r', y='frac_ir', color='blue', ax=ax, label='Fraction Individual')
+    plt.legend();
+    plt.title('Oakland County, Republican Votes')
+    
     plt.figure(2)
     ax = plt.gca()
-    cons.plot(kind='scatter', x='frac_d', y='frac_sd', color='red', ax=ax)
-    cons.plot(kind='scatter', x='frac_d', y='frac_id', color='blue', ax=ax)
+    cons.plot(kind='scatter', x='frac_d', y='frac_sd', color='red', ax=ax, label='Fraction Straight Ticket')
+    cons.plot(kind='scatter', x='frac_d', y='frac_id', color='blue', ax=ax, label='Fraction Individual')
+    plt.legend();
+    plt.title('Oakland County, Democrat Votes')    
+
+    (bay_i, bay_s) = load_our_data('../source-data/bay-county-mi-11-13-2020/')
+    bay_cons = build_plots(bay_i, bay_s)
+
+    plt.figure(3)
+    ax = plt.gca()
+    bay_cons.plot(kind='scatter', x='frac_r', y='frac_sr', color='red', ax=ax, label='Fraction Straight Ticket')
+    bay_cons.plot(kind='scatter', x='frac_r', y='frac_ir', color='blue', ax=ax, label='Fraction Individual')
+    plt.legend();
+    plt.title('Bay County, Republican Votes')
+
+    plt.figure(4)
+    ax = plt.gca()
+    bay_cons.plot(kind='scatter', x='frac_d', y='frac_sd', color='red', ax=ax, label='Fraction Straight Ticket')
+    bay_cons.plot(kind='scatter', x='frac_d', y='frac_id', color='blue', ax=ax, label='Fraction Individual')
+    plt.legend();
+    plt.title('Bay County, Democrat Votes')
+    
     plt.show()
 
     
